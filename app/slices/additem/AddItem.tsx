9c8e7e662f9efed543fe281dfda28971b7fa9c50
Jeff Romine <jeff@nightheron.dev>
2025-10-06 17:19:55 -0600

    solution

--- app/slices/additem/AddItem.tsx
+++ app/slices/additem/AddItem.tsx
@@ -1,36 +1,49 @@
-import {useEffect, useState} from "react";
-import {findEventStore, subscribeStream} from "@/app/infrastructure/inmemoryEventstore";
-import {addItemCommandHandler} from "@/app/slices/additem/commandHandler";
-import {v4} from "uuid";
-import { Streams } from "@/app/api/Streams";
-import {CartEvents} from "@/app/api/events/CartEvents";
-import AddItemTests from "@/app/slices/additem/AddItemTests";
+import { useEffect, useState } from 'react';
+import { findEventStore, subscribeStream } from '@/app/infrastructure/inmemoryEventstore';
+import { addItemCommandHandler } from '@/app/slices/additem/commandHandler';
+import { v4 } from 'uuid';
+import { Streams } from '@/app/api/Streams';
+import { CartEvents } from '@/app/api/events/CartEvents';
+import AddItemTests from '@/app/slices/additem/AddItemTests';
 
 export type Product = {
-    name: string,
-    price: number,
-    description: string,
-    productId: string
-}
+    name: string;
+    price: number;
+    description: string;
+    productId: string;
+};
 
-export default function AddItem(props: {aggregateId: string, selectedProduct: Product}) {
-
-    return <div>
-        <div className={"control"}>
-            <button onClick={async () => {
-
-                    //let result = await findEventStore().readStream<CartEvents>(Streams.Cart)
-                    //let events = result?.events || []
-                    /*let resultEvents = await addItemCommandHandler(events, {
-                        type: 'AddItem', data: {
-                           //..
-                        }
-                    })*/
-                    /*await findEventStore().appendToStream(Streams.Cart, resultEvents,
-                        {expectedStreamVersion: result?.currentStreamVersion})*/
-                }
-
-            } className={"button is-info m-2"}>Add Item</button>
+export default function AddItem(props: { aggregateId: string; selectedProduct: Product }) {
+    return (
+        <div>
+            <div className={'control'}>
+                <button
+                    onClick={async () => {
+                        // step 1 - read current state from the event store
+                        let result = await findEventStore().readStream<CartEvents>(Streams.Cart);
+                        let events = result?.events || [];
+                        // step 2 - call command handler to get new events
+                        let resultEvents = await addItemCommandHandler(events, {
+                            type: 'AddItem',
+                            data: {
+                                aggregateId: props.aggregateId,
+                                name: props.selectedProduct.name,
+                                productId: props.selectedProduct.productId,
+                                description: props.selectedProduct.description,
+                                price: props.selectedProduct.price,
+                                itemId: v4(),
+                            },
+                        });
+                        // step 3 - append new events to the event store
+                        await findEventStore().appendToStream(Streams.Cart, resultEvents, {
+                            expectedStreamVersion: result?.currentStreamVersion,
+                        });
+                    }}
+                    className={'button is-info m-2'}
+                >
+                    Add Item
+                </button>
+            </div>
         </div>
-    </div>
+    );
 }
--- app/slices/additem/commandHandler.ts
+++ app/slices/additem/commandHandler.ts
@@ -1,16 +1,48 @@
-import {CartEvents} from "@/app/api/events/CartEvents";
-import {AddItemCommand} from "@/app/api/commands/AddItemCommand";
-import {ItemAddedEvent} from "@/app/api/events/ItemAddedEvent";
-import {findEventStore} from "@/app/infrastructure/inmemoryEventstore";
+import { AddItemCommand } from '@/app/api/commands/AddItemCommand';
+import { CartEvents } from '@/app/api/events/CartEvents';
+import { ItemAddedEvent } from '@/app/api/events/ItemAddedEvent';
 
-export const addItemCommandHandler = async (events: CartEvents[], command: AddItemCommand): Promise<CartEvents[]> => {
+export const addItemCommandHandler = async (
+    events: CartEvents[],
+    command: AddItemCommand,
+): Promise<CartEvents[]> => {
+    const itemsInCart = events.reduce((acc, event) => {
+        if (event.type === 'ItemAdded') {
+            acc.push(event);
+        } else if (event.type === 'ItemArchived') {
+            acc = acc.filter((item) => item.data.itemId !== event.data.itemId);
+        } else if (event.type === 'ItemRemoved') {
+            acc = acc.filter((item) => item.data.itemId !== event.data.itemId);
+        } else if (event.type === 'CartCleared') {
+            acc = [];
+        }
+        return acc;
+    }, [] as ItemAddedEvent[]);
+
+    if (itemsInCart.length >= 3) {
+        throw new Error('Cannot add more than 3 items');
+    }
+    // business rule: cannot add same product twice
+    const itemAlreadyInCart = itemsInCart.find(
+        (item) =>
+            item.data.productId === command.data.productId &&
+            item.data.aggregateId === command.data.aggregateId,
+    );
+    if (itemAlreadyInCart) {
+        throw new Error('Item already in cart');
+    }
 
     return [
-        /*{
-        type: 'ItemAdded',
-        data: {
-             TODO apply items
-        }
-    }*/]
-
-}
+        {
+            type: 'ItemAdded',
+            data: {
+                name: command.data.name,
+                itemId: command.data.itemId,
+                price: command.data.price,
+                description: command.data.description,
+                productId: command.data.productId,
+                aggregateId: command.data.aggregateId,
+            },
+        },
+    ];
+};
