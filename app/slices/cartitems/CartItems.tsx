9404fb9bb3a659858e2aad3955ba8d7ad3224b3f
Jeff Romine <jeff@nightheron.dev>
2025-10-06 18:42:16 -0600

    solution

--- app/slices/cartitems/CartItems.tsx
+++ app/slices/cartitems/CartItems.tsx
@@ -1,65 +1,78 @@
-import {findEventStore, subscribeStream, unsubscribeStream} from "@/app/infrastructure/inmemoryEventstore";
-import {useEffect, useState} from "react";
-import {Streams} from "@/app/api/Streams";
-import {CartItem, cartItemsStateView} from "@/app/slices/cartitems/CartItemsStateView";
-import {CartEvents} from "@/app/api/events/CartEvents";
-import RemoveItem from "../removeitem/RemoveItem";
+import {
+    findEventStore,
+    subscribeStream,
+    unsubscribeStream,
+} from '@/app/infrastructure/inmemoryEventstore';
+import { useEffect, useState } from 'react';
+import { Streams } from '@/app/api/Streams';
+import { CartItem, cartItemsStateView } from '@/app/slices/cartitems/CartItemsStateView';
+import { CartEvents } from '@/app/api/events/CartEvents';
+import RemoveItem from '../removeitem/RemoveItem';
 
 export default function CartItems(props: { aggregateId: string }) {
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-    useEffect(() => {
+    /*     useEffect(() => {
         (async () => {
             const result = await findEventStore().readStream<CartEvents>(Streams.Cart)
             let cartState = cartItemsStateView(cartItems ?? [], result?.events || [])
             setCartItems(cartState)
         })()
     }, []);
-
+ */
     //TODO switch to a stream subscription
-    /*useEffect(() => {
-        let subscription = subscribeStream(Streams.Cart, (nextExpectedStreamVersion, events: CartEvents[],) => {
-            setCartItems((prevState) => {
-                return cartItemsStateView(prevState, events)
-            })
-        })
+    useEffect(() => {
+        let subscription = subscribeStream(
+            Streams.Cart,
+            (_nextExpectedStreamVersion, events: CartEvents[]) => {
+                setCartItems((prevState) => {
+                    return cartItemsStateView(prevState, events);
+                });
+            },
+        );
         return () => {
-            unsubscribeStream(Streams.Cart, subscription)
-        }
-    }, []);*/
+            unsubscribeStream(Streams.Cart, subscription);
+        };
+    }, []);
 
     return (
         <div className="box">
             <h3 className="title is-4">Shopping Cart</h3>
 
             {cartItems.length === 0 ? (
-                <div className="notification is-light">
-                    Your cart is empty
-                </div>
+                <div className="notification is-light">Your cart is empty</div>
             ) : (
                 <>
                     <div className="table-container">
                         <table className="table is-fullwidth is-striped">
                             <thead>
-                            <tr>
-                                <th>Product</th>
-                                <th>Price</th>
-                            </tr>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Price</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            {cartItems.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.name}</td>
-                                    <td>${item.price.toFixed(2)}</td>
-                                    <td><RemoveItem aggregateId={item.aggregateId} itemId={item.itemId}/></td>
-                                </tr>
-                            ))}
+                                {cartItems.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>${item.price.toFixed(2)}</td>
+                                        <td>
+                                            <RemoveItem
+                                                aggregateId={item.aggregateId}
+                                                itemId={item.itemId}
+                                            />
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                             <tfoot>
-                            <tr>
-                                <th colSpan={3} className="has-text-right">Total:</th>
-                                <th>${cartItems.reduce((acc, data) => acc += data.price, 0)}</th>
-                            </tr>
+                                <tr>
+                                    <th colSpan={3} className="has-text-right">
+                                        Total:
+                                    </th>
+                                    <th>
+                                        ${cartItems.reduce((acc, data) => (acc += data.price), 0)}
+                                    </th>
+                                </tr>
                             </tfoot>
                         </table>
                     </div>
@@ -75,5 +88,5 @@ export default function CartItems(props: { aggregateId: string }) {
                 </>
             )}
         </div>
-    )
+    );
 }
--- app/slices/removeitem/commandHandler.ts
+++ app/slices/removeitem/commandHandler.ts
@@ -1,27 +1,38 @@
-import {CartEvents} from "@/app/api/events/CartEvents";
-import {AddItemCommand} from "@/app/api/commands/AddItemCommand";
-import {ItemAddedEvent} from "@/app/api/events/ItemAddedEvent";
-import {findEventStore} from "@/app/infrastructure/inmemoryEventstore";
-import {RemoveItemCommand} from "@/app/api/commands/RemoveItemCommand";
+import { CartEvents } from '@/app/api/events/CartEvents';
+import { AddItemCommand } from '@/app/api/commands/AddItemCommand';
+import { ItemAddedEvent } from '@/app/api/events/ItemAddedEvent';
+import { findEventStore } from '@/app/infrastructure/inmemoryEventstore';
+import { RemoveItemCommand } from '@/app/api/commands/RemoveItemCommand';
 
-export const removeItemCommandHandler =
-    async (events: CartEvents[], command: RemoveItemCommand): Promise<CartEvents[]> => {
-
-    var itemsInCart = []
-
+export const removeItemCommandHandler = async (
+    events: CartEvents[],
+    command: RemoveItemCommand,
+): Promise<CartEvents[]> => {
     // TODO rebuild cart state from events
     // TODO throw error if item to be removed is not in cart
-
+    const itemsInCart = events.reduce((acc: string[], event: CartEvents) => {
+        if (event.type === 'ItemAdded') {
+            acc.push(event.data.itemId);
+        } else if (event.type === 'ItemRemoved') {
+            acc = acc.filter((itemId) => itemId !== event.data.itemId);
+        } else if (event.type === 'ItemArchived') {
+            acc = acc.filter((itemId) => itemId !== event.data.itemId);
+        } else if (event.type == 'CartCleared') {
+            acc = [];
+        }
+        return acc; // Return the updated accumulator
+    }, []);
     /*if (itemsInCart.indexOf(command.data.itemId) == -1) {
         throw new Error("Item is not in cart");
     }*/
 
-    return [{
-        type: 'ItemRemoved',
-        data: {
-            aggregateId: command.data.aggregateId,
-            itemId: command.data.itemId,
-        }
-    }]
-
-}
+    return [
+        {
+            type: 'ItemRemoved',
+            data: {
+                aggregateId: command.data.aggregateId,
+                itemId: command.data.itemId,
+            },
+        },
+    ];
+};
